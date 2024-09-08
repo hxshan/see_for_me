a4c480b1d9c2b92150543a81c35a3de41d3d6f8d
Heshan Gimhana <galgodageheshan@gmail.com>
2024-09-08 19:57:03 +0530

    Minor:API Test 1 (Sucessful)

---
+++ lib/data/tile.dart
@@ -0,0 +1,15 @@
+class Tile {
+  final int x;
+  final int y;
+  final String type;
+
+  Tile({required this.x, required this.y, required this.type});
+
+  factory Tile.fromJson(Map<String, dynamic> json) {
+    return Tile(
+      x: json['x'],
+      y: json['y'],
+      type: json['type'],
+    );
+  }
+}
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:see_for_me/data/store_map.dart';
+import 'package:see_for_me/data/tile.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,6 +18,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final FlutterTts flutterTts = FlutterTts();
   final SpeechToText _speechToText = SpeechToText();
+  List<List<Tile?>> _mapGrid = [];
 
   bool _speechEnabled = false;
   String wordsSpoken = "";
@@ -22,6 +27,14 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     _initSpeech();
+    _fetchAndSetMapGrid();
+  }
+
+  Future<void> _fetchAndSetMapGrid() async {
+    List<List<Tile?>> mapGrid = await createMapGrid();
+    setState(() {
+      _mapGrid = mapGrid; // Set the map grid after fetching
+    });
   }
 
   void _initSpeech() async {
@@ -60,6 +73,30 @@ class _HomePageState extends State<HomePage> {
     await flutterTts.setLanguage("en-US");
     await flutterTts.setPitch(1);
     await flutterTts.speak(text);
+  }
+
+  Future<StoreMap> fetchMap() async {
+    final uri = Uri.parse('http://10.0.2.2:5224/api/floor/1');
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      return StoreMap.fromJson(json.decode(response.body));
+    }
+    throw Exception('Failed to load map');
+  }
+
+  Future<List<List<Tile?>>> createMapGrid() async {
+    try {
+      StoreMap storeMap = await fetchMap();
+      List<List<Tile?>> grid = List.generate(
+          storeMap.height, (x) => List.generate(storeMap.width, (x) => null));
+      for (Tile tile in storeMap.tiles) {
+        grid[tile.x][tile.y] = tile;
+      }
+
+      return grid;
+    } catch (e) {
+      return [];
+    }
   }
 
   @override
