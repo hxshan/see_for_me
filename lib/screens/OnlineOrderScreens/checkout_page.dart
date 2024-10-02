537663489633632d2363fe1179f6e7694561392c
Heshan Gimhana <galgodageheshan@gmail.com>
2024-10-02 21:13:11 +0530

    rename user to client

--- lib/screens/OnlineOrderScreens/checkout_page.dart
+++ lib/screens/OnlineOrderScreens/checkout_page.dart
@@ -4,12 +4,10 @@ import 'package:flutter_tts/flutter_tts.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:shared_preferences/shared_preferences.dart'; //shared preference
-import 'package:see_for_me/models/cartItem.dart';//cart class
+import 'package:see_for_me/models/cartItem.dart'; //cart class
 import 'dart:convert'; // For json.decode
 import 'package:see_for_me/ordering/searchResponses.dart';
-import 'package:see_for_me/models/user.dart';
-
-
+import 'package:see_for_me/models/client.dart';
 
 class CheckoutPage extends StatefulWidget {
   const CheckoutPage({super.key});
@@ -21,7 +19,6 @@ class CheckoutPage extends StatefulWidget {
 class _CheckoutPageState extends State<CheckoutPage> {
   final FlutterTts flutterTts = FlutterTts();
   final SpeechToText _speechToText = SpeechToText();
-  
 
   bool _speechEnabled = false;
   String wordsSpoken = "";
@@ -29,7 +26,7 @@ class _CheckoutPageState extends State<CheckoutPage> {
 
   List<Item> items = List.empty(growable: true);
 
-  final User user = User(
+  final Client user = Client(
     id: "U12345",
     name: "John Doe",
     address: "123 Main St, Cityville",
@@ -37,26 +34,26 @@ class _CheckoutPageState extends State<CheckoutPage> {
     phoneNumber: "+1234567890",
   );
 
-    void _loadCartItemsFromSharedPreferences() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  List<String>? storedItems = prefs.getStringList('myCart');
-  
-  if (storedItems != null) {
-    setState(() {
-      // Decoding JSON strings into Item objects
-      items = storedItems
-          .map((itemString) => Item.fromJson(json.decode(itemString)))
-          .toList();
-    });
-  } else {
-    // If no items found in SharedPreferences, set items to an empty list
-    setState(() {
-      items = [];
-    });
-  }
-}
+  void _loadCartItemsFromSharedPreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String>? storedItems = prefs.getStringList('myCart');
 
- String getResponse(String key) {
+    if (storedItems != null) {
+      setState(() {
+        // Decoding JSON strings into Item objects
+        items = storedItems
+            .map((itemString) => Item.fromJson(json.decode(itemString)))
+            .toList();
+      });
+    } else {
+      // If no items found in SharedPreferences, set items to an empty list
+      setState(() {
+        items = [];
+      });
+    }
+  }
+
+  String getResponse(String key) {
     if (key.contains("searching")) {
       for (var response in helpResponses) {
         if (response.containsKey(key)) {
@@ -74,14 +71,13 @@ class _CheckoutPageState extends State<CheckoutPage> {
     }
   }
 
-      void announceCurrentPage(String pageName) {
-  // Construct the response to inform the user which page they are on
-  response = "You are currently on the $pageName page.";
-  
-  // Use the text-to-speech feature to speak the response
-  speak(response);
-}
+  void announceCurrentPage(String pageName) {
+    // Construct the response to inform the user which page they are on
+    response = "You are currently on the $pageName page.";
 
+    // Use the text-to-speech feature to speak the response
+    speak(response);
+  }
 
   @override
   void initState() {
@@ -107,7 +103,7 @@ class _CheckoutPageState extends State<CheckoutPage> {
 
   void processCommand(SpeechRecognitionResult result) {
     String spokenWords = result.recognizedWords.toLowerCase();
-     if (spokenWords.contains("repeat")) {
+    if (spokenWords.contains("repeat")) {
       if (response.isNotEmpty) {
         speak(response); // Repeat the last response
       } else {
@@ -120,11 +116,11 @@ class _CheckoutPageState extends State<CheckoutPage> {
       return;
     }
 
-    if(spokenWords.contains("price")){
-    promptTotalPrice();
-  } else if(spokenWords.contains("current page")){
-    announceCurrentPage("Checkout");
-  } else {
+    if (spokenWords.contains("price")) {
+      promptTotalPrice();
+    } else if (spokenWords.contains("current page")) {
+      announceCurrentPage("Checkout");
+    } else {
       response = "Say again";
       speak(response); // Speak the response
     }
@@ -135,21 +131,21 @@ class _CheckoutPageState extends State<CheckoutPage> {
   }
 
   void promptTotalPrice() {
-  double totalPrice = 0.0;
+    double totalPrice = 0.0;
 
-  // Calculate total price since all items will have a quantity of at least 1
-  for (var item in items) {
-    totalPrice += item.quantity! * item.price;
+    // Calculate total price since all items will have a quantity of at least 1
+    for (var item in items) {
+      totalPrice += item.quantity! * item.price;
+    }
+
+    // Construct the response to inform the user about the total price
+    response =
+        "The total price of all items in your cart is \$${totalPrice.toStringAsFixed(2)}.";
+
+    speak(response);
   }
 
-  // Construct the response to inform the user about the total price
-  response = "The total price of all items in your cart is \$${totalPrice.toStringAsFixed(2)}.";
-  
-  speak(response);
-}
-
-
-   void _onSpeechResult(SpeechRecognitionResult result) {
+  void _onSpeechResult(SpeechRecognitionResult result) {
     // Only process the command when the user has finished speaking
     if (result.finalResult) {
       processCommand(result);
@@ -162,131 +158,132 @@ class _CheckoutPageState extends State<CheckoutPage> {
     await flutterTts.speak(text);
   }
 
-@override
-Widget build(BuildContext context) {
-  double totalPrice = 0.0;
-  
-  // Calculate total price for all items in the cart
-  for (var item in items) {
-    totalPrice += item.price * (item.quantity ?? 1);
-  }
+  @override
+  Widget build(BuildContext context) {
+    double totalPrice = 0.0;
 
-  return Scaffold(
-    appBar: AppBar(
-      title: Text("Checkout Page"),
-    ),
-    backgroundColor: Colors.white,
-    body: Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // User details section
-          Text(
-            "User Details",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 10),
-          Text("ID: ${user.id}", style: TextStyle(fontSize: 18)),
-          Text("Name: ${user.name}", style: TextStyle(fontSize: 18)),
-          Text("Address: ${user.address}", style: TextStyle(fontSize: 18)),
-          Text("Email: ${user.email}", style: TextStyle(fontSize: 18)),
-          Text("Phone: ${user.phoneNumber}", style: TextStyle(fontSize: 18)),
-          SizedBox(height: 20),
+    // Calculate total price for all items in the cart
+    for (var item in items) {
+      totalPrice += item.price * (item.quantity ?? 1);
+    }
 
-          // Cart items section
-          Text(
-            "Cart Items",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 10),
-
-          Expanded(
-            child: items.isNotEmpty
-                ? ListView.builder(
-                    itemCount: items.length,
-                    itemBuilder: (context, index) {
-                      final item = items[index];
-                      return Card(
-                        margin: EdgeInsets.symmetric(vertical: 8.0),
-                        child: ListTile(
-                          title: Text(item.name),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("Price: \$${item.price.toStringAsFixed(2)}"),
-                              Text("Quantity: ${item.quantity ?? 1}"),
-                            ],
-                          ),
-                          trailing: Text(
-                            "Total: \$${(item.price * (item.quantity ?? 1)).toStringAsFixed(2)}",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  )
-                : Center(
-                    child: Text(
-                      "Your cart is empty",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-          ),
-
-          // Total price section at the end
-          SizedBox(height: 20),
-          Text(
-            "Total Price: \$${totalPrice.toStringAsFixed(2)}",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
-          ),
-          
-          SizedBox(height: 20),
-          Center(
-            child: Container(
-              width: 120,
-              height: 120,
-              decoration: BoxDecoration(
-                color: Colors.purple[300],
-                shape: BoxShape.circle,
-              ),
-              child: IconButton(
-                icon: Icon(Icons.mic, size: 60, color: Colors.black),
-                onPressed: _speechToText.isListening
-                    ? _stopListening
-                    : _startListening,
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          Center(
-            child: Text(
-              "wordsSpoken: $wordsSpoken",
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Checkout Page"),
+      ),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // User details section
+            Text(
+              "User Details",
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-        ],
+            SizedBox(height: 10),
+            Text("ID: ${user.id}", style: TextStyle(fontSize: 18)),
+            Text("Name: ${user.name}", style: TextStyle(fontSize: 18)),
+            Text("Address: ${user.address}", style: TextStyle(fontSize: 18)),
+            Text("Email: ${user.email}", style: TextStyle(fontSize: 18)),
+            Text("Phone: ${user.phoneNumber}", style: TextStyle(fontSize: 18)),
+            SizedBox(height: 20),
+
+            // Cart items section
+            Text(
+              "Cart Items",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+
+            Expanded(
+              child: items.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: items.length,
+                      itemBuilder: (context, index) {
+                        final item = items[index];
+                        return Card(
+                          margin: EdgeInsets.symmetric(vertical: 8.0),
+                          child: ListTile(
+                            title: Text(item.name),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                    "Price: \$${item.price.toStringAsFixed(2)}"),
+                                Text("Quantity: ${item.quantity ?? 1}"),
+                              ],
+                            ),
+                            trailing: Text(
+                              "Total: \$${(item.price * (item.quantity ?? 1)).toStringAsFixed(2)}",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    )
+                  : Center(
+                      child: Text(
+                        "Your cart is empty",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+            ),
+
+            // Total price section at the end
+            SizedBox(height: 20),
+            Text(
+              "Total Price: \$${totalPrice.toStringAsFixed(2)}",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+
+            SizedBox(height: 20),
+            Center(
+              child: Container(
+                width: 120,
+                height: 120,
+                decoration: BoxDecoration(
+                  color: Colors.purple[300],
+                  shape: BoxShape.circle,
+                ),
+                child: IconButton(
+                  icon: Icon(Icons.mic, size: 60, color: Colors.black),
+                  onPressed: _speechToText.isListening
+                      ? _stopListening
+                      : _startListening,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: Text(
+                "wordsSpoken: $wordsSpoken",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
